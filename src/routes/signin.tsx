5095b542359f72bf9b07de5f2ebbce2fdb1b3aa1
Ali Bassiouni <aabassiouni@hotmail.com>
2025-02-06 02:11:15 +0200

    add react query

--- src/routes/signin.tsx
+++ src/routes/signin.tsx
@@ -1,3 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+
 export function SigninPage() {
-  return <div>SigninPage</div>;
+  const { data } = useQuery({
+    queryKey: ["signin"],
+    queryFn: async () => {
+      return await fetch("https://api.github.com/users/aabassiouni", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((res) => res.json());
+    },
+  });
+
+  return <div>{JSON.stringify(data)}</div>;
 }
